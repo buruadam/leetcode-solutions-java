4cc0ea13075344a1a524daade1169f58ce45e9e7
buruadam <adam.buru@gmail.com>
2026-06-25 13:23:33 +0200

    Add: #268 Missing Number solution

---
+++ src/easy/MissingNumber.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
