aff9715cb23415743a528ae92c48d525242c82ae
buruadam <adam.buru@gmail.com>
2026-06-26 12:48:45 +0200

    Add: #98 Validate Binary Search Tree solution

---
+++ src/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,43 @@
+package medium;
+
+public class ValidateBinarySearchTree {
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode node, Long min, Long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (min != null && node.val <= min) {
+            return false;
+        }
+
+        if (max != null && node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, (long) node.val) &&
+                validate(node.right, (long) node.val, max);
+    }
+}
