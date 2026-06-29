6f3c81b476b4a6de0d5279070c5b2e1f843e048e
buruadam <adam.buru@gmail.com>
2026-06-29 13:14:50 +0200

    Add: #141 Linked List Cycle solution

---
+++ src/easy/LinkedListCycle.java
@@ -0,0 +1,33 @@
+package easy;
+
+public class LinkedListCycle {
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
