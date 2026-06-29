98c8ebdf55ebb9899068f591c41a31cef50dd9c2
buruadam <adam.buru@gmail.com>
2026-06-29 12:35:22 +0200

    Add: #21 Merge Two Sorted Lists solution

---
+++ src/easy/MergeTwoSortedLists.java
@@ -0,0 +1,49 @@
+package easy;
+
+import common.ListNode;
+
+public class MergeTwoSortedLists {
+    // 1. Iterative solution
+    public ListNode mergeTwoListsIterative(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    // 2. Recursive solution
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoListsRecursive(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoListsRecursive(list1, list2.next);
+            return list2;
+        }
+    }
+}
